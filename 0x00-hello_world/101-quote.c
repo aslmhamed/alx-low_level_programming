fda264751779e8ed03129da1e9d13b8595f539ce
aslmhamed <aslamhamed999@gmail.com>
2023-07-13 18:41:20 -0700

    ${var}

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,11 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints while not using printf or puts.
- *
- *Return: Always 1 (Success)
-*/
-
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-}	return (1);
+	write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
