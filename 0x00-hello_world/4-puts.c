c008fcbbf3c892f4139ad62a52486e83c82d4169
aslmhamed <aslamhamed999@gmail.com>
2023-07-13 17:50:26 -0700

    ${var}

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
-int main (void){
 
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that prints we puts function
+ *
+ * Return: Always 0(Success)
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
-
+	return (0);
 }
