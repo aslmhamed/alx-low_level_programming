9e2cab5d73730974b15b3903f6d620bc9c7b177c
aslmhamed <aslamhamed999@gmail.com>
2023-08-04 18:11:26 -0700

    ${var}

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints it`s name
+ * @argc: int.
+ * @argv: list
+ * Return: Always 0.
+ */
+
+int main(int argc, char const **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
