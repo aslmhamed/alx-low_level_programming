d9f81739ade674e7d8e3f77d3c115dfd1a7c3483
aslmhamed <aslamhamed999@gmail.com>
2023-08-15 16:59:35 -0700

    All tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Description: a program tha prints that file direction
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LIKE_MACRO_H_
+#define _FUNCTION_LIKE_MACRO_H_
+
+#define ABC(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif
