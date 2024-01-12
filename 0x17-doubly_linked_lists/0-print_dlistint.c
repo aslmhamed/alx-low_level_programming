5a4d91d04a10f16585e51109c774847bf774db0c
aslmhamed <aslamhamed999@gmail.com>
2024-01-11 16:41:58 -0800

    fixed betty errors :(

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- * print_dlistint - prints all elements of the node
+ * print_dlistint - prints the number of nodes.
  * @h: head of the list.
  *
- * Return: Number of nodes.
+ * Return: the number of elements.
+ *
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t i;
-  
-  for (i = 0; h; i++)
-  {
-      printf("%d\n", h->n);
-      h = h->next;
-  }
-    
-}
+	size_t i;
 
+	for (i = 0; h; i++)
+	{
+		print("%d\n", h->n);
+		h = h->next;
+	}
+}
